4808a53b31c4f1f6d53d81667cd7e51a58c22731
root <damianemerah1@gmail.com>
2023-08-17 02:27:48 +0000

    Advanced task Basic auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -14,7 +14,9 @@ class Auth:
         if excluded_paths is None or not excluded_paths:
             return True
         for excluded_path in excluded_paths:
-            if excluded_path.startswith(path):
+            if excluded_path.endswith('*') and path.startswith(excluded_path[:-1]):
+                return False
+            elif excluded_path == path:
                 return False
         return True
 
